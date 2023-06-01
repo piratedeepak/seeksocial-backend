27ea4a191b2cd1d462745fb2bf3ea22ae1fd32a1
Deepak Sharma <deepak.sharma.98@outlook.com>
2023-06-01 23:48:17 +0530

    isAuthenticated fixed

--- seekSocial/middlewares/authMiddleware.js
+++ seekSocial/middlewares/authMiddleware.js
@@ -1,34 +1,41 @@
-import { User } from '../models/userModel.js'
-import jwt from 'jsonwebtoken'
-import { responseCommon } from '../../utils/resComm.js';
+import { User } from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import { responseCommon } from "../../utils/resComm.js";
+
+import jwt from 'jsonwebtoken';
 
 export const isAuthenticated = async (req, res, next) => {
   try {
     const authToken = req.headers.authorization;
+    const refreshToken = req.headers['x-refresh-token'];
 
-  if(!authToken) return responseCommon(res, 401,undefined, null, false)
+    if (!authToken && !refreshToken) {
+      return responseCommon(res, 401, 'Unauthorized', null, false);
+    }
 
     const accessToken = authToken.split(' ')[1];
 
-  if(!accessToken) return responseCommon(res, 401, undefined, null, false)
-
-
-  const decode = jwt.verify(accessToken, process.env.ACCESS_SECRET)
-
-  if(!decode) return responseCommon(res, 401, undefined, null, false )
-
-    req.user = await User.findById(decode.id)
-    next()
-  } catch (error) {
-    const errorMessage = error.message
-    if(errorMessage === "jwt expired") {
-      return responseCommon(res, 403, undefined, null, false)
-    } else if (errorMessage === "invalid signature") {
-      return responseCommon(res, 401, undefined, null, false)
+    if (!accessToken) {
+      return responseCommon(res, 401, 'Unauthorized', null, false);
     }
 
-    console.error(error.message)
-    responseCommon(res, 500, "Internal Server Error", null, false)
+    try {
+      const decode = jwt.verify(accessToken, process.env.ACCESS_SECRET);
+      req.user = await User.findById(decode.id);
+      next();
+    } catch (error) {
+      if (error instanceof jwt.TokenExpiredError) {
+        return responseCommon(res, 403, 'Access token expired', null, false);
+      } else if (error instanceof jwt.JsonWebTokenError) {
+        return responseCommon(res, 401, 'Invalid access token', null, false);
+      }
+
+      console.error(error.message);
+      responseCommon(res, 500, 'Internal Server Error', null, false);
+    }
+  } catch (error) {
+    console.error(error.message);
+    responseCommon(res, 500, 'Internal Server Error', null, false);
   }
-}
+};
 
