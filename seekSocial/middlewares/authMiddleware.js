4d6e3c601201ab37814dfaaf059a37b30c4ed170
piratedeepak <deepak.sharma.98@outlook.com>
2023-06-01 08:06:02 +0000

    formatted and remove await

--- seekSocial/middlewares/authMiddleware.js
+++ seekSocial/middlewares/authMiddleware.js
@@ -1,22 +1,22 @@
 import { User } from '../models/userModel.js'
-import  jwt  from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 import { responseCommon } from '../../utils/resComm.js';
 
-export const isAuthenticated = async(req, res, next) => {
+export const isAuthenticated = async (req, res, next) => {
   try {
     const authToken = req.headers.authorization;
 
-  if(!authToken) return responseCommon(res, 401, "User Not Logged In", null, false)
+    if (!authToken) return responseCommon(res, 401, "User Not Logged In", null, false)
 
-  const accessToken = authToken.split(' ')[1];
+    const accessToken = authToken.split(' ')[1];
 
-  if(!accessToken) return responseCommon(res, 401, "Unauthorized", null, false)
+    if (!accessToken) return responseCommon(res, 401, "Unauthorized", null, false)
 
 
-  const decode = await jwt.verify(accessToken, process.env.ACCESS_SECRET)
+    const decode = jwt.verify(accessToken, process.env.ACCESS_SECRET)
 
-  req.user = await User.findById(decode.id)
-  next()
+    req.user = await User.findById(decode.id)
+    next()
   } catch (error) {
     responseCommon(res, 500, "Internal Server Error", null, false)
   }
