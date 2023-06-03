8bf75211daf07cd0cede9d31403d27981ea9ee49
pratham2912 <pratham@kaabilfinance.org>
2023-06-03 13:42:14 +0530

    .

--- seekSocial/controllers/v1/users/userController.js
+++ seekSocial/controllers/v1/users/userController.js
@@ -1,36 +1,49 @@
 import getElasticClient from "../../../../config/elasticsearch.js";
 import { sendTokens } from "../../../../utils/generateTokens.js";
 import { userService } from "../../../services/v1/users/userService.js"
+import { Strategy as GoogleStrategy } from 'passport-google-oauth2';
+import passport from 'passport';
 import { responseCommon } from "../../../../utils/resComm.js";
+
 
 export const login = async (req, res, next) => {
   try {
     const user = await userService.login(req.body);
-    const tokens = sendTokens(user);
+    sendTokens(user, res);
 
-    return responseCommon(res, 200, "Login Successfully", tokens, true)
-
+    return res.status(200).json({
+      success:true,
+      message:"Login Successfully",
+      data: user,
+    });
   } catch (error) {
-    // console.log(error)
-    return responseCommon(res, 400, error.message, null, false)
+    console.log(error)
+    return res.status(400).json({ 
+      success:false,
+      message: error.message });
   }
 };
 
 export const register = async (req, res, next) => {
   try {
     const user = await userService.register(req.body);
-    const tokens = sendTokens(user)
+    sendTokens(user, res)
 
-    return responseCommon(res, 200, "Registered Successfully", tokens, true)
-
+    return res.status(200).json({
+      success:true,
+      message: "Register Successfully",
+      data: user,
+    });
   } catch (error) {
-    return responseCommon(res, 400, error.message, null, false)
+    return res.status(400).json({ 
+      success:false,
+      message: error.message });
   }
 };
 
 export const refreshTheToken = async (req, res, next) => {
   try {
-    const refreshToken = req.headers['x-refresh-token'];
+    const refreshToken = req.cookies["refreshToken"];
 
     const accessToken = await userService.refreshTheToken(refreshToken);
 
@@ -39,11 +52,35 @@ export const refreshTheToken = async (req, res, next) => {
       maxAge: 15 * 24 * 60 * 60 * 1000,
     };
 
-    return responseCommon(res, 200, "Access token has been refreshed", accessToken, true)
-
+    res.cookie("accessToken", accessToken, options);
+    res.status(200).json({
+      message: "Access token has been refreshed",
+      success:true
+    });
   } catch (error) {
-    return responseCommon(res, 400, error.message, null, false)
+    return res.status(401).json({
+      success:false,
+       message: error.message 
+      });
+  }
+};
 
+export const logout = async (req, res, next) => {
+  try {
+    const options = { maxAge: 0 };
+
+    res.cookie("accessToken", null, options);
+    res.cookie("refreshToken", null, options);
+
+    res.status(200).json({
+      success: true,
+      message:"Logout Successfully"
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success:false,
+      message:error.message
+    });
   }
 };
 
@@ -51,10 +88,14 @@ export const getProfile = async (req, res, next) => {
   try {
     const user = await userService.getProfile(req.user._id);
 
-    return responseCommon(res, 200, "User Data Found", user, true)
-
+    res.status(200).json({
+      success: true,
+      user,
+    });
   } catch (error) {
-    return responseCommon(res, 400, error.message, null, false)
+    return res.status(400).json({ 
+      success:false,
+      message: error.message });
   }
 };
 export const filterUserData = async (req, res) => {
@@ -62,11 +103,32 @@ export const filterUserData = async (req, res) => {
     const data = req.body;
     const client = await getElasticClient();
     const response = await userService.filterService(client, data);
-
-    return responseCommon(res, 200, null, response.hits, true)
+    res.status(200).json({
+      success:true,
+      data:response.hits
+    });
   } catch (error) {
-    return responseCommon(res, 500, error.message, null, false)
+    console.error(error);
+    res.status(500).json({ 
+      success:false,
+      message:error.message
+     });
   }
+};
+
+export const googleLogout = (req, res, next) => {
+
+  const options = { maxAge: 0 };
+
+  res.cookie("accessToken", null, options);
+  res.cookie("refreshToken", null, options);
+
+  req.session.destroy((err)=>{
+      if(err) return next(err)
+
+      res.clearCookie('connect.sid')
+      res.status(200).redirect(process.env.FRONTEND_URL)
+  })
 }
 
 export const forgetPassword = async(req, res, next) => {
@@ -74,10 +136,14 @@ export const forgetPassword = async(req, res, next) => {
 
     const response = await userService.forgetPassword(req.body)
     
-    return responseCommon(res, 200, "Email has been sent successfully", response, true)
-
+    res.status(200).json({
+      success:true,
+      message:"Email has been sent successfully",
+      response
+    })
   } catch (error) {
-    return responseCommon(res, 500, error.message, null, false)
+    console.error(error)
+    res.status(500).json({error: error.message})
   }
 }
 
@@ -89,22 +155,22 @@ export const changePassword = async (req, res, next) => {
 
     const response = await userService.changePassword(token, password)
 
-    return responseCommon(res, 200, "Password Updated Successfully", response, true)
-
+    res.status(200).json({
+      success: true,
+      message: "Password Updated"
+    })
   } catch (error) {
     console.error(error)
-    return responseCommon(res, 500, error.message, null, false)
+    res.status(500).json({ error: error.message })
   }
   
 }
 
 
 export const sendGoogleLoginToken = async (req, res) => {
-<<<<<<< HEAD
 
-  console.log(req.tokens)
-=======
->>>>>>> e4a474c1b576513f100aa4fd0e17fe174dc9f91f
+  const tokens = req.user
+  // console.log(tokens)
  try{
   res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     return responseCommon(res, 200, "Login Successfully", tokens, true)
